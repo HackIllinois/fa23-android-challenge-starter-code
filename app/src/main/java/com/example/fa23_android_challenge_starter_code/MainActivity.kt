d40e9a93ed0867b198558ed57d105843caee0628
Rachel Shum <rshum2@illinois.edu>
2022-10-07 01:05:18 -0500

    add starter code and comments

--- app/src/main/java/com/example/fa23_android_challenge_starter_code/MainActivity.kt
+++ app/src/main/java/com/example/fa23_android_challenge_starter_code/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.fa23_android_challenge_starter_code
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/* Create an event info page that displays an event's details.
+ * Make a GET API call to the HackIllinois API event endpoint
+ * Recommended: use Retrofit to create HTTP requests
+ * Remember to add the libraries you want to use to your build.gradle file!
+*/
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
